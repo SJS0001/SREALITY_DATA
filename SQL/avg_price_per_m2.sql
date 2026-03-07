afadceb0a97c2eceadbc57c56d35ee7978a5ef14
SJS0001 <58920132+SJS0001@users.noreply.github.com>
2026-03-07 09:08:42 +0100

    Delete SQL/avg_price_per_m2.sql

--- SQL/avg_price_per_m2.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REFRESH MATERIALIZED VIEW price_per_m2_table AS
-SELECT
-    cityPart,
-    AVG(price_per_m2) as avg_price
-FROM workspace.default.sreality_gold
-GROUP BY cityPart
-ORDER BY avg_price DESC
